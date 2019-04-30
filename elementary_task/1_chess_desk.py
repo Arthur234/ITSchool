b3da0d0fc8426ed7291f6b97291f37731434a65e
Arthur Yatsun <ayatsun@ukr.net>
2019-04-30 05:43:35 +0300

    First task

---
+++ elementary_task/1_chess_desk.py
@@ -0,0 +1,28 @@
+from elementary_task.helper.controller import Controller
+
+
+class ChessDesk(Controller):
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+        self.desk = ''
+
+        if str(self.height).isnumeric() and str(self.width).isnumeric():
+            self.create()
+        elif self.height == '' and self.width == '':
+            Controller.instruction('Enter height than enter width')
+        else:
+            Controller.wrong_parameters_alert()
+
+    def create(self):
+        for h in range(self.height):
+            if h % 2 == 1:
+                self.desk += ' '
+
+            self.desk += ' '.join('*' * self.width) + '\n'
+
+        print(self.desk)
+
+
+if __name__ == '__main__':
+    ChessDesk(50, 4)
---
+++ elementary_task/helper/controller.py
@@ -0,0 +1,9 @@
+class Controller:
+
+    @staticmethod
+    def wrong_parameters_alert(message='Wrong input data'):
+        print(message)
+
+    @staticmethod
+    def instruction(instruction=''):
+        print(instruction)
