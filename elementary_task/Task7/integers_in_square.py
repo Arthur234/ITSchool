3fb91e671249d1918702278a6b1541c7d9f233d8
Arthur543 <arthur54342@gmail.com>
2019-05-19 09:52:45 +0300

    Task7

---
+++ elementary_task/Task7/integers_in_square.py
@@ -0,0 +1,33 @@
+class Integers:
+    def __init__(self, n):
+        self.number = n
+
+    def _convert_to_int(self):
+        try:
+            self.number = int(self.number)
+        except ValueError:
+            raise ValueError('Invalid type: {}'.format(
+                type(self.number)
+            ))
+
+    def count_integers(self):
+        self._convert_to_int()
+        numbers = []
+        max_square = int(self.number ** 0.5)
+        for i in range(max_square):
+            numbers.append(i)
+
+        return numbers
+
+
+if __name__ == '__main__':
+    while True:
+        number = input('Enter number: ')
+
+        integers = Integers(number)
+        print(integers.count_integers())
+
+        is_break = True if input('Continue? [y/n]: ') == 'n' else False
+        if is_break:
+            print('Good luck! \')')
+            break
